1c31616b808bb6be129dd8f295beebc513d49ed0
zollty <zollty@163.com>
2015-03-24 00:59:56 +0800

    update

--- public/js/base.js
+++ public/js/base.js
@@ -82,6 +82,16 @@ $(document).ready(function() {
     });
   });
   contentEffects();
+  
+  var duoshuoQuery = {short_name:$('.ds-thread').attr('data-name')};
+  (function() {
+	var ds = document.createElement('script');
+	ds.type = 'text/javascript';ds.async = true;
+	ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+	ds.charset = 'UTF-8';
+	(document.getElementsByTagName('head')[0] 
+	 || document.getElementsByTagName('body')[0]).appendChild(ds);
+  })();
 });
 function contentEffects(){
   //remove the asidebar
