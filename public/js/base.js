56fd9a5496938bc64f56e7bce2e7ded2548ecaed
zollty <zollty@163.com>
2015-04-03 09:48:49 +0800

    update

--- public/js/base.js
+++ public/js/base.js
@@ -100,7 +100,7 @@ function addDuoShuo(){
 function contentEffects(){
   //remove the asidebar
   $('.row-offcanvas').removeClass('active');
-  if($("#nav").length > 0 && document.getElementById("nav").innerHTML!='' ){
+  if($("#nav").length > 0 && $("#nav").html()!="" ){
     $("#content > h2,#content > h3,#content > h4,#content > h5,#content > h6").each(function(i) {
         var current = $(this);
         current.attr("id", "title" + i);
